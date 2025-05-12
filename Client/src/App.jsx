8accccad859de42363d86fc7ba45061551cddf56
ch-ankit679 <srishttverma14@gmail.com>
2025-05-13 00:42:03 +0530

    files

---
+++ Client/src/App.jsx
@@ -0,0 +1,178 @@
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import Square from './Square/Square'
+import {io} from 'socket.io-client';
+import Swal from 'sweetalert2';
+
+
+const renderFrom = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+const App = () => {
+
+  const [gameState, setGameState] = useState(renderFrom);
+  const [currentPlayer, setCurrentPlayer] = useState('circle');
+  const [finishedState, setFinishedState] = useState(false);
+  const [finishedArrayState, setFinishedArrayState] = useState([]);
+  const [playOnline, setPlayOnline] = useState(false);
+  const [socket , setSocket] = useState(null);
+  const [playerName, setPlayerName] = useState('');
+  const [opponentName, setOpponentName] = useState(null);
+  const [playingAs, setPlayingAs] = useState(null);
+
+  const checkWinner = () => {
+    //Row dynamic
+    for (let row = 0; row < gameState.length; row++) {
+      if (gameState[row][0] === gameState[row][1] && gameState[row][1] === gameState[row][2]) {
+        setFinishedArrayState([row*3+0,row*3+1,row*3+2]);
+        return gameState[row][0];
+      }
+    }
+    //Column dynamic
+    for (let col = 0; col < gameState.length; col++) {
+      if (gameState[0][col] === gameState[1][col] && gameState[1][col] === gameState[2][col]) {
+        setFinishedArrayState([0*3+col,1*3+col,2*3+col]);
+        return gameState[0][col];
+      }
+    }
+
+    if(gameState[0][0] === gameState[1][1] && gameState[1][1] === gameState[2][2]){
+      setFinishedArrayState([0,4,8]);
+      return gameState[0][0];
+    }
+
+    if(gameState[0][2] === gameState[1][1] && gameState[1][1] === gameState[2][0]){
+      setFinishedArrayState([2,4,6]);
+      return gameState[1][1];
+    }
+
+    const isDrawMatch = gameState.flat().every((e) =>{
+      if(e === 'circle' || e==='cross') return true;
+    });
+
+    if(isDrawMatch) return 'draw';
+
+    return null;
+  }
+
+  useEffect(() => {
+    const winner = checkWinner();
+    if(winner){
+      setFinishedState(winner);
+    }
+  }, [gameState]);
+
+  const takePlayerName = async() =>{
+    const result = await Swal.fire({
+      title: "Enter your name",
+      input: "text",
+      inputLabel: "Enter your name",
+      showCancelButton: true,
+      inputValidator: (value) => {
+        if (!value) {
+          return "You need to write something!";
+        }
+      }
+    });
+    return result;
+  };
+
+  socket?.on("OpponentLeftMatch",()=>{
+    alert("Opponent left the Match");
+    setFinishedState("OpponentLeftMatch");
+  });
+
+  socket?.on("playerMoveFromServer",(data) => {
+    // setCurrentPlayer(data.state.currentPlayer);
+    const id = data.state.id;
+    setGameState((prevState) => {
+      let newState = [...prevState];  
+      const rowIndex = Math.floor(id/3);
+      const colIndex = id % 3;
+      newState[rowIndex][colIndex] = data.state.sign;
+      return newState;
+    });
+    setCurrentPlayer(data.state.sign === "circle" ? "cross":"circle");
+  })
+
+  socket?.on("connect", function(){
+    setPlayOnline(true);
+  });
+
+  socket?.on("OpponentNotFound", function(){
+    setOpponentName(false);
+  });
+
+  socket?.on("OpponentFound", function(data){
+    setPlayingAs(data.playingAs);
+    setOpponentName(data.opponentName);
+  });
+
+   async function playOnlineClick(){
+    const result = await takePlayerName();
+
+    if(!result.isConfirmed){
+      return;
+    }
+
+    const username = result.value;
+    setPlayerName(username);
+
+    const newSocket = io('http://localhost:3000',{
+      autoConnect : true,
+    });
+    newSocket?.emit("request_to_play",{
+      playerName: username,
+    });
+    setSocket(newSocket);
+  }
+   
+  if(!playOnline){
+    return <div className='main-div'>
+      <button onClick={playOnlineClick} className='playOnline'>Play Online</button>
+    </div>
+  }
+
+  if(playOnline && !opponentName){
+    return( 
+      <div className='waiting'>
+       <p>Waiting for Opponent...</p>
+      </div>
+    )
+  }
+
+  return (
+    <div className='main-div'>
+      <div className='move-detection'>
+        <div className={`left ${currentPlayer === playingAs ? 'current-move-' + currentPlayer : ''}`}>{playerName}</div>
+        <div className={`right ${currentPlayer !== playingAs ? 'current-move-' + currentPlayer : ''}`}>{opponentName}</div>
+      </div>
+      <div>
+        <h1 className='game-heading water-background'>Tic-Tac-Toe</h1>
+        <div className='square-wrapper'>
+          {
+            gameState.map((arr, rowIndex) =>
+              arr.map((e, colIndex) => {
+                return <Square playingAs = {playingAs} currentElement = {e} gameState = {gameState} socket = {socket} finishedArrayState = {finishedArrayState} finishedState={finishedState} currentPlayer={currentPlayer} setCurrentPlayer={setCurrentPlayer} setGameState={setGameState} id={rowIndex * 3 + colIndex} key={rowIndex * 3 + colIndex} />
+              })
+            )
+          }
+        </div>
+        {
+          finishedState && finishedState != 'OpponentLeftMatch' && finishedState !== 'draw' &&(<h3 className='finished-state'>{finishedState === playingAs ? "You ": finishedState} won the game</h3>)
+        }
+        {
+          finishedState && finishedState != 'OpponentLeftMatch' && finishedState === 'draw' &&(<h3 className='finished-state'>Its a Draw!</h3>)
+        }
+      </div>
+        {
+          finishedState && finishedState === 'OpponentLeftMatch' &&(<h2>You won the match, Opponent left!!</h2>)
+        }
+    </div>
+  )
+}
+
+export default App
---
+++ Server/server.js
@@ -0,0 +1,88 @@
+// import { createServer } from "http";
+// import { Server } from "socket.io";
+
+const { createServer } = require("http");
+const { Server } = require('socket.io');
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: "http://localhost:5173/",
+});
+
+const allUsers = {};
+const allRooms = [];
+
+io.on("connection", (socket) => {
+  
+  allUsers[socket.id] = {
+    socket: socket,
+    online: true,
+  };
+
+  socket.on("request_to_play", (data) => {
+    const currentUser = allUsers[socket.id];
+    currentUser.playerName = data.playerName;
+
+    let opponentPlayer;
+    for (const key in allUsers) {
+      const user = allUsers[key];
+      if(user.online && !user.playing && socket.id !== key){
+        opponentPlayer = user;
+        break;        
+      }
+    }
+
+    if(opponentPlayer){
+      
+      allRooms.push({
+        player1:opponentPlayer,
+        player2:currentUser,
+      });
+
+      opponentPlayer.socket.emit("OpponentFound",{
+        opponentName: currentUser.playerName,
+        playingAs:"circle",
+      });
+      currentUser.socket.emit("OpponentFound",{
+        opponentName: opponentPlayer.playerName,
+        playingAs:"cross",
+      });
+
+      currentUser.socket.on("playerMoveFromClient",(data) => {
+        opponentPlayer.socket.emit("playerMoveFromServer",{
+          ...data
+        });
+      });
+      opponentPlayer.socket.on("playerMoveFromClient",(data) => {
+        currentUser.socket.emit("playerMoveFromServer",{
+          ...data
+        });
+      });
+    }else{
+      currentUser.socket.emit("OpponentNotFound");
+    }
+  });
+
+  socket.on("disconnect", function () {
+    const currentUser = allUsers[socket.id];
+    currentUser.online = false;
+    currentUser.playing = false;
+
+    for (let index = 0; index < allRooms.length; index++) {
+      const {player1,player2} = allRooms[index];
+
+      if(player1.socket.id === socket.id){
+        player2.socket.emit("OpponentLeftMatch");
+        break;
+      }
+
+      if(player2.socket.id === socket.id){
+        player1.socket.emit("OpponentLeftMatch");
+        break;
+      }
+      
+    }
+  });
+});
+
+httpServer.listen(3000);
